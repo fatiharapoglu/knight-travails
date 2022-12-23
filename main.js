ae4f56009dbbdfed1d2ee03d8386a8635a4536af
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-23 23:44:33 +0300

    prettier

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class Knight {
 
         const distance = new Array(8).fill(0).map(() => new Array(8).fill(-1));
         const path = new Array(8).fill(0).map(() => new Array(8).fill([]));
-        
+
         distance[start[0]][start[1]] = 0;
         path[start[0]][start[1]] = [start];
 
@@ -24,7 +24,7 @@ class Knight {
 
             for (let move of moves) {
                 const newPos = [current[0] + move[0], current[1] + move[1]];
-                
+
                 if (this.isValid(newPos, visited)) {
                     queue.push(newPos);
                     distance[newPos[0]][newPos[1]] = distance[current[0]][current[1]] + 1;
@@ -32,17 +32,31 @@ class Knight {
                 }
             }
         }
-    }
+    };
 
     static getMoves = () => {
-        const moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
+        const moves = [
+            [-2, -1],
+            [-2, 1],
+            [-1, -2],
+            [-1, 2],
+            [1, -2],
+            [1, 2],
+            [2, -1],
+            [2, 1],
+        ];
         return moves;
-    }
+    };
 
     static isValid = (pos, visited) => {
-        const boo = pos[0] >= 0 && pos[0] < 8 && pos[1] >= 0 && pos[1] < 8 && !visited.some(unit => unit[0] === pos[0] && unit[1] === pos[1]);
+        const boo =
+            pos[0] >= 0 &&
+            pos[0] < 8 &&
+            pos[1] >= 0 &&
+            pos[1] < 8 &&
+            !visited.some((unit) => unit[0] === pos[0] && unit[1] === pos[1]);
         return boo;
-    }
+    };
 }
 
 const start = [0, 0];
