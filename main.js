e34a7cdff0d62f1c94c0faf2a2bd12e474cb5496
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-23 23:35:58 +0300

    merge js files

--- main.js
+++ main.js
@@ -1,63 +1,51 @@
 class Knight {
     static move = (start, end) => {
-        let q = [];
-        q.push(start);
+        const queue = [start];
+        const visited = [];
 
-        let visited = new Set();
-        visited.add(start);
+        const distance = new Array(8).fill(0).map(() => new Array(8).fill(-1));
+        const path = new Array(8).fill(0).map(() => new Array(8).fill([]));
+        
+        distance[start[0]][start[1]] = 0;
+        path[start[0]][start[1]] = [start];
 
-        let dist = 0;
+        while (queue.length > 0) {
+            const current = queue.shift();
 
-        while (q.length > 0) {
-            let size = q.length;
+            if (current[0] === end[0] && current[1] === end[1]) {
+                return {
+                    distance: distance[current[0]][current[1]],
+                    path: path[current[0]][current[1]],
+                };
+            }
 
-            for (let i = 0; i < size; i++) {
-                let current = q.shift();
+            visited.push(current);
+            const moves = this.getMoves(current);
 
-                if (current[0] == end[0] && current[1] == end[1]) {
-                    return dist;
-                }
-
-                let moves = this.getMoves(current);
-
-                for (let move of moves) {
-                    if (this.isValid(move) && !visited.has(move)) {
-                        q.push(move);
-                        visited.add(move);
-                    }
+            for (let move of moves) {
+                const newPos = [current[0] + move[0], current[1] + move[1]];
+                
+                if (this.isValid(newPos, visited)) {
+                    queue.push(newPos);
+                    distance[newPos[0]][newPos[1]] = distance[current[0]][current[1]] + 1;
+                    path[newPos[0]][newPos[1]] = path[current[0]][current[1]].concat([newPos]);
                 }
             }
-            dist++;
         }
     }
 
-    static getMoves = (pos) => {
-        let x = pos[0];
-        let y = pos[1];
-        let moves = [
-            [x + 2, y + 1],
-            [x + 2, y - 1],
-            [x - 2, y + 1],
-            [x - 2, y - 1],
-            [x + 1, y + 2],
-            [x + 1, y - 2],
-            [x - 1, y + 2],
-            [x - 1, y - 2]
-        ];
+    static getMoves = () => {
+        const moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
         return moves;
     }
 
-    static isValid = (pos) => {
-        let x = pos[0];
-        let y = pos[1];
-        let boo = x >= 0 && x < 8 && y >= 0 && y < 8;
+    static isValid = (pos, visited) => {
+        const boo = pos[0] >= 0 && pos[0] < 8 && pos[1] >= 0 && pos[1] < 8 && !visited.some(unit => unit[0] === pos[0] && unit[1] === pos[1]);
         return boo;
     }
 }
 
-let start = [0, 0];
-let end = [1, 2];
+const start = [0, 0];
+const end = [7, 7];
 
-let distance = Knight.move(start, end);
-
-console.log(distance);
+console.log(Knight.move(start, end));
